1b2aa2c8345dd1077ee68f7a8e86ccd6114f92b4
Truong Nguyen <samuraitruong@hotmail.com>
2016-09-12 21:17:58 +1000

    'prototype'

--- gruntfile.js
+++ gruntfile.js
@@ -2,16 +2,39 @@ module.exports = function(grunt) {
   grunt.initConfig({
     ts: {
       client : {
-        src: ["client/*/*.ts", "!node_modules/**"],
+        src: ["typings/*","client/*/*.ts", "!node_modules/**"],
         out:'dist/client/scripts/site.js'
       },
       server : {
-        src: ["server/**/*.ts", "!node_modules/**"],
+        src: ["typings/*","server/**/*.ts", "!node_modules/**"],
         out:'dist/server/app.js'
+      }
+    },
+
+   tslint: {
+      options: {
+        configuration: "tslint.json"
+      },
+      files: {
+        src: ["src/**/*.ts"]
+      }
+    },
+    watch: {
+      ts: {
+        files: ["js/src/**/*.ts", "src/**/*.ts"],
+        tasks: ["ts", "tslint"]
       }
     }
   });
+
+  grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-ts");
-  grunt.registerTask("default", ["ts"]);
+  grunt.loadNpmTasks("grunt-tslint");
+
+  grunt.registerTask("default", [
+    "ts",
+    "tslint"
+  ]);
+  //grunt.registerTask("default", ["ts"]);
 };
 //https://github.com/TypeStrong/grunt-ts/tree/master/sample
