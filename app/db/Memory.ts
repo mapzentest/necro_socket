3d6197509957f06737c6e094991945a73fc16142
Truong Nguyen <samuraitruong@hotmail.com>
2016-09-20 00:35:17 +1000

    Dirty code -> need refactor later, add dropbox sync to collect data for statistic

--- app/db/Memory.ts
+++ app/db/Memory.ts
@@ -6,56 +6,92 @@ import * as moment from 'moment'
 var node_dropbox = require('node-dropbox')
 var configs = require('../config/config.json')
 var pokemons = require('../config/pokemons.json')
+var Dropbox = require('dropbox');
+
 
 class Memory implements IPogoDatabase {
     private data: IPokemonItem[] = [];
     private dropbox: any;
     private all: any = [];
-    private counter: any = 0;
+    private counter: any = 201000; //current value....
+    private stats: any = {}
     private pokemonSettings: IPokemonBasic[];
     constructor() {
         this.pokemonSettings = [];
         let accessToken = '6uT8iPrMZWwAAAAAAAAAJqQ7SxCUIdPkidHXmbTq9PZyKGePBLv4n9c7EG3wcqq_'
-        this.dropbox = node_dropbox.api(accessToken);
+        this.dropbox = new Dropbox({ accessToken: accessToken });
+        this.loadSyncedData()
+    }
+
+    public storeSyncedData = (obj: any): void => {
+        if (obj) {
+            this.dropbox.filesUpload(
+                {
+                    path: '/stats.txt',
+                    contents: JSON.stringify(obj),
+                    mode: {
+                        '.tag': 'overwrite'
+                    }
+                })
+                .then(function (response) {
+                    console.log('data file stored on dropbox');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+
+        }
+    }
+    public loadSyncedData = (): void => {
+        let me = this;
+        this.dropbox.filesDownload(
+            {
+                path: '/stats.txt',
+                '.tag': 'path'
+            })
+            .then(function (response) {
+                me.stats = JSON.parse(response.fileBinary);
+            })
+            .catch(function (error) {
+                me.stats = {
+                    totals: 0,
+                    pokemons: {}
+                }
+            });
+
     }
     public addPokemon = (p: IPokemonItem): boolean => {
-        this.counter++;
-
-        if (configs.DropboxSync && this.counter % configs.BatchSize == 0) {
-            this.dropbox.createFile(`stat.log`, this.counter, function (err, res, body) {
-                // body...
-                console.log('dropbox file synced.')
-            });
-        }
-
-        /*this.all.push(p);
-
-        if(this.all.length == configs.BatchSize) {
-            this.dropbox.createFile(`${(new Date()).getTime()}.json`, this.all , function aaa(err, res, body) {
-                // body...
-                console.log('dropbox file synced.')
-            }); // Creates a new file.
-            this.all = []
-        }
-        */
         let pokemon = pokemons[p.PokemonId];
+        if (!pokemon) return;
+        p.Rarity = pokemon.Rarity;
+        p.Name = pokemon.Name;
 
-        if (pokemon) {
-            p.Rarity = pokemon.Rarity;
-            p.Name = pokemon.Name;
+        this.counter++;
+        if (this.stats) {
+            if (!this.stats.pokemons[p.Name]) this.stats.pokemons[p.Name] = {
+                Id: p.PokemonId,
+                Count: 0
+            };
+            this.stats.pokemons[p.Name].Count = this.stats.pokemons[p.Name].Count + 1;
+            this.stats.totals = this.stats.totals + 1;
+        }
 
-            if (!configs.UseFilter || pokemon.Feed && p.IV >= pokemon.FilteredIV) {
-                var checkexist = this.data.filter((f) => {
-                    return p.EncounterId == f.EncounterId;
-                });
-                if (checkexist && checkexist.length > 0) return false;
+        if (configs.DropboxSync && (this.counter = configs.BatchSize)) {
+            this.counter = 0;
+            this.storeSyncedData(this.stats)
+        }
 
-                this.data.push(p);
-                return true;
-            }
-            else {
-                console.log(`Ignored: ${p.Name} | ${p.IV} | ${p.Rarity}`)
-            }
+        if (!configs.UseFilter || pokemon.Feed && p.IV >= pokemon.FilteredIV) {
+            var checkexist = this.data.filter((f) => {
+                return p.EncounterId == f.EncounterId;
+            });
+            if (checkexist && checkexist.length > 0) return false;
+
+            this.data.push(p);
+            return true;
+        }
+        else {
+            console.log(`Ignored: ${p.Name} | ${p.IV} | ${p.Rarity}`)
         }
         return false;
     }
